7a84475671125ce355d71ae8ee58dd6e133ff53b
HeyMoon <deng9085@163.com>
2021-07-07 22:56:56 +0800

    2.4 pq

---
+++ src/main/java/com/dyh/algorithms4/chapter2/MinPQ.java
@@ -0,0 +1,160 @@
+package com.dyh.algorithms4.chapter2;
+
+import java.util.NoSuchElementException;
+
+/**
+ * @author: dengyunhui
+ * @datetime: 2021/7/7 22:38
+ * @description:
+ */
+public class MinPQ<Key extends Comparable<Key>> {
+
+    private Key[] pq;                    // store items at indices 1 to n
+    private int n;                       // number of items on priority queue
+
+    public MinPQ(int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        n = 0;
+    }
+
+    public MinPQ() {
+        this(1);
+    }
+
+    /**
+     * Initializes a priority queue from the array of keys.
+     * <p>
+     * Takes time proportional to the number of keys, using sink-based heap construction.
+     *
+     * @param keys the array of keys
+     */
+    public MinPQ(Key[] keys) {
+        n = keys.length;
+        pq = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < n; i++)
+            pq[i + 1] = keys[i];
+        for (int k = n / 2; k >= 1; k--)
+            sink(k);
+        assert isMinHeap();
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns a smallest key on this priority queue.
+     *
+     * @return a smallest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key min() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+
+    // resize the underlying array to have the given capacity
+    private void resize(int capacity) {
+        assert capacity > n;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+    /**
+     * Adds a new key to this priority queue.
+     *
+     * @param x the key to add to this priority queue
+     */
+    public void insert(Key x) {
+        // double size of array if necessary
+        if (n == pq.length - 1) resize(2 * pq.length);
+
+        // add x, and percolate it up to maintain heap invariant
+        pq[++n] = x;
+        swim(n);
+        assert isMinHeap();
+    }
+
+    /**
+     * Removes and returns a smallest key on this priority queue.
+     *
+     * @return a smallest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key delMin() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        Key min = pq[1];
+        exch(1, n--);
+        sink(1);
+        pq[n + 1] = null;     // to avoid loitering and help with garbage collection
+        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
+        assert isMinHeap();
+        return min;
+    }
+
+
+    /***************************************************************************
+     * Helper functions to restore the heap invariant.
+     ***************************************************************************/
+
+    private void swim(int k) {
+        while (k > 1 && greater(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && greater(j, j + 1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    /***************************************************************************
+     * Helper functions for compares and swaps.
+     ***************************************************************************/
+    private boolean greater(int i, int j) {
+        return pq[i].compareTo(pq[j]) > 0;
+    }
+
+    private void exch(int i, int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+
+    // is pq[1..n] a min heap?
+    private boolean isMinHeap() {
+        for (int i = 1; i <= n; i++) {
+            if (pq[i] == null) return false;
+        }
+        for (int i = n + 1; i < pq.length; i++) {
+            if (pq[i] != null) return false;
+        }
+        if (pq[0] != null) return false;
+        return isMinHeapOrdered(1);
+    }
+
+    // is subtree of pq[1..n] rooted at k a min heap?
+    private boolean isMinHeapOrdered(int k) {
+        if (k > n) return true;
+        int left = 2 * k;
+        int right = 2 * k + 1;
+        if (left <= n && greater(k, left)) return false;
+        if (right <= n && greater(k, right)) return false;
+        return isMinHeapOrdered(left) && isMinHeapOrdered(right);
+    }
+
+
+}
