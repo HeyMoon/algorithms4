b3a213e9bbae80a0b5264ee0f4300e1bdea46a77
HeyMoon <deng9085@163.com>
2021-08-18 22:25:46 +0800

    median of two sorted array

--- src/main/java/com/dyh/algorithms4/leetcode/LongestSubstring.java
+++ src/main/java/com/dyh/algorithms4/leetcode/LongestSubstring.java
@@ -6,4 +6,36 @@ package com.dyh.algorithms4.leetcode;
  * @description:
  */
 public class LongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int[] occur = new int[256];
+        for (int i = 0; i < 256; i++) {
+            occur[i] = -1;
+        }
+
+        int begin = 0;
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            begin = Math.max(begin, occur[c] + 1);
+            length = Math.max(length, i - begin + 1);
+            occur[c] = i;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+        System.out.println(longestSubstring.lengthOfLongestSubstring(" "));
+        System.out.println(longestSubstring.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(longestSubstring.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(longestSubstring.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(longestSubstring.lengthOfLongestSubstring("bfbv"));
+    }
+
 }
--- src/main/java/com/dyh/algorithms4/leetcode/MedianOfTwoSortedArrays.java
+++ src/main/java/com/dyh/algorithms4/leetcode/MedianOfTwoSortedArrays.java
@@ -3,7 +3,43 @@ package com.dyh.algorithms4.leetcode;
 /**
  * @author: dengyunhui
  * @datetime: 2021/8/18 21:40
- * @description:
+ * @description: 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
  */
 public class MedianOfTwoSortedArrays {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1 == null && nums2 == null) {
+            return 0.0;
+        }
+
+        int[] merge = new int[nums1.length + nums2.length];
+        int i = 0, j = 0;
+        while (true) {
+            if (i >= nums1.length) {
+                merge[i + j] = nums2[j++];
+            } else if (j >= nums2.length) {
+                merge[i + j] = nums1[i++];
+            } else if (nums1[i] < nums2[j]) {
+                merge[i + j] = nums1[i++];
+            } else {
+                merge[i + j] = nums2[j++];
+            }
+
+            if (i >= nums1.length && j >= nums2.length) {
+                break;
+            }
+        }
+
+        if (merge.length % 2 == 0) {
+            return (merge[merge.length / 2] + merge[merge.length / 2 - 1]) / 2.0;
+        } else {
+            return merge[merge.length / 2];
+        }
+    }
+
+    public static void main(String[] args) {
+        MedianOfTwoSortedArrays medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
+        double median = medianOfTwoSortedArrays.findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0});
+        System.out.println(median);
+    }
 }
